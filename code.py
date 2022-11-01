1d57ce810af4272ed82517a8d44244f6aa4be436
matthewScohen <64049126+matthewScohen@users.noreply.github.com>
2022-11-01 13:11:10 -0400

    add code.py

---
+++ code.py
@@ -0,0 +1,68 @@
+import board
+import microcontroller
+
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+
+from adafruit_airlift.esp32 import ESP32
+
+# If you are using a Metro M4 Airlift Lite, PyPortal,
+# or MatrixPortal, you can use the default pin settings.
+# Leave this DEFAULT line uncommented.
+# esp32 = ESP32() # DEFAULT
+
+# If you are using CircuitPython 6.0.0 or earlier,
+# on PyPortal and PyPortal Titano only, use the pin settings
+# below. Comment out the DEFAULT line above and uncomment
+# the line below. For CircuitPython 6.1.0, the pin names
+# have changed for these boards, and the DEFAULT line
+# above is correct.
+# esp32 = ESP32(tx=board.TX, rx=board.RX)
+
+# If you are using an AirLift FeatherWing or AirLift Bitsy Add-On,
+# use the pin settings below. Comment out the DEFAULT line above
+# and uncomment the lines below.
+# If you are using an AirLift Breakout, check that these
+# choices match the wiring to your microcontroller board,
+# or change them as appropriate.
+esp32 = ESP32(  
+    reset=board.D12,
+    gpio0=board.D10,
+    busy=board.D11,
+    chip_select=board.D13,
+    tx=board.TX,
+    rx=board.RX,
+)
+
+# If you are using an AirLift Shield,
+# use the pin settings below. Comment out the DEFAULT line above
+# and uncomment the lines below.
+# esp32 = ESP32(
+#     reset=board.D5,
+#     gpio0=board.D6,
+#     busy=board.D7,
+#     chip_select=board.D10,
+#     tx=board.TX,
+#     rx=board.RX,
+# )
+
+adapter = esp32.start_bluetooth()
+
+ble = BLERadio(adapter)
+uart = UARTService()
+advertisement = ProvideServicesAdvertisement(uart)
+
+print(f"Saved phone number : {microcontroller.nvm[0:10]}") # https://docs.circuitpython.org/en/latest/shared-bindings/nvm/index.html
+while True:
+    ble.start_advertising(advertisement)
+    print("waiting to connect")
+    while not ble.connected:
+        pass
+    print("connected: trying to read input")
+    while ble.connected:
+        # Returns b'' if nothing was read.
+        phone_number = uart.read(10)
+        if phone_number:
+            print(phone_number)
+            microcontroller.nvm[0:10] = phone_number
