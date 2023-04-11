ea081d68f5f1d3241e7d69ee3039857b1ddfcfbe
Tiffany De Faria <70450373+tiffdefaria@users.noreply.github.com>
2023-04-11 14:17:20 -0400

    Updated RPi with interrupt for IMU

--- sim7600.py
+++ sim7600.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import serial
 import time
+import signal
+import sys
 
 ser = serial.Serial("/dev/ttyS0",115200)
 ser.flushInput()
@@ -8,6 +10,19 @@ phone_number = '9542583115'
 power_key = 6
 rec_buff = ''
 time_count = 0
+BUTTON_GPIO = 16
+button_pressed = False
+
+# Interrupts
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+    
+def button_pressed_callback(channel):
+    button_pressed = True
+    print("bike stolen")
+    text_message = 'Your bike has been moved!'
+    SendShortMessage(phone_number,text_message)
 
 # Supporting functions
 def send_at(command,back,timeout):
@@ -33,7 +48,7 @@ def send_at_GPS(command,back,timeout):
     ser.write((command+'\r\n').encode())
     time.sleep(timeout)
     if ser.inWaiting():
-        time.sleep(0.01)
+        time.sleep(5)
         rec_buff = ser.read(ser.inWaiting())
     if rec_buff != '':
         if back not in rec_buff.decode():
@@ -86,13 +101,34 @@ def SendShortMessage(phone_number,text_message):
     if 1 == answer:
         ser.write(text_message.encode())
         ser.write(b'\x1A')
-        answer = send_at('','OK',20)
+        answer = send_at('','OK',5)
         if 1 == answer:
             print('send successfully')
         else:
             print('error')
     else:
         print('error%d'%answer)
+    
+def get_location(response):
+    #Get the location from the GPS
+    latitude = response.split('\n')[1].split(',')[0].strip('+CGPSINFO: ')
+    N_or_S = response.split('\n')[1].split(',')[1]
+    longitude = response.split('\n')[1].split(',')[2]
+    E_or_W = response.split('\n')[1].split(',')[3]
+    #Set the coordinates into standard GPS
+    latitude = latitude.replace(".","")
+    longitude = longitude.replace(".","")
+    latitude = f"{latitude[:2]}.{latitude[2:]}"
+    longitude = f"{longitude[:3]}.{longitude[3:]}"
+    
+    lat = latitude + ' ' + N_or_S
+    if longitude[0] == '0':
+        long = longitude[1:] + ' ' + E_or_W
+    else:
+        long = longitude + ' ' + E_or_W
+    
+    
+    return lat, long
 
 def get_gps_position():
     rec_null = True
@@ -103,25 +139,25 @@ def get_gps_position():
     send_at_GPS('AT+CGPS=1,1','OK',1)
     time.sleep(2)
     while rec_null:
-        answer = send_at_GPS('AT+CGPSINFO','+CGPSINFO: ',1)
+        answer = send_at_GPS('AT+CGPSINFO','+CGPSINFO: ',5)
         response = rec_buff.decode()
         if 1 == answer:
             answer = 0
-            #Get the location from the GPS
-            lat = response.split('\n')[1].split(',')[0].strip('+CGPSINFO: ')
-            long = response.split('\n')[1].split(',')[2]
-            return lat, long
-        
-            if ',,,,,,' in rec_buff:
+            if ',,,,,,' in rec_buff.decode():
                 print('GPS is not ready')
-                rec_null = False
+                rec_null = True
                 time.sleep(1)
+            else:
+                location = get_location(response)
+                lat = location[0]
+                long = location[1]
+                return lat, long
         else:
             print('error %d'%answer)
             rec_buff = ''
             send_at_GPS('AT+CGPS=0','OK',1)
             return False
-        time.sleep(0.5)
+        time.sleep(1.5)
 
 def power_on(power_key):
     print('SIM7600X is starting:')
@@ -149,18 +185,29 @@ def power_down(power_key):
 try:
     power_on(power_key)
     
+    # Check for IMU alert
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
+    GPIO.add_event_detect(BUTTON_GPIO,GPIO.FALLING,
+                          callback=button_pressed_callback, bouncetime=100)
+    signal.signal(signal.SIGINT, signal_handler)
+            
     while True:
-        # Clear the SMS cache
-        #send_at('AT+CMGD=,2',"OK",1)
         # Receive a text from a phone to set the phone number for alerts
         msg = ReceiveShortMessage()
+        
+        # Clear the SMS cache
+        send_at('AT+CMGD=,2',"OK",1)
         
         if msg == 'Set phone number':   
             # Reply to that phone to confirm
             text_message = 'Phone number set!'
             SendShortMessage(phone_number,text_message)
             
-        if msg == 'Find my bike': 
+        if msg == 'Find my bike':
+            text_message = 'Locating bike...'
+            SendShortMessage(phone_number,text_message)
             # Get GPS location
             gps_loc = get_gps_position()
             latitude = gps_loc[0]
@@ -170,6 +217,7 @@ try:
             msg = ReceiveShortMessage()
             # Text location to phone when requested
             SendShortMessage(phone_number,text_message)
+            
         if msg == 'Power off':
             text_message = 'Turning off device...'
             SendShortMessage(phone_number,text_message)
@@ -180,5 +228,3 @@ except :
     if ser != None:
         ser.close()
     GPIO.cleanup()
-
-
